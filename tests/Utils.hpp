0181d31b3c96c9fc6d70bd2b852b621f93955856
romintomasetti <romin.tomasetti@gmail.com>
2024-09-05 16:57:33 +0000

    hpx(tests): add test for bulk adaptor

--- tests/Utils.hpp
+++ tests/Utils.hpp
@@ -11,6 +11,17 @@ auto get_thread_id() {
     return std::hash<std::thread::id>{}(std::this_thread::get_id());
 }
 
+//! Simple functor that fills a @c std::vector with the thread ID.
+struct FillWithThreadID
+{
+    std::shared_ptr<std::vector<size_t>> ids;
+
+    template <std::integral T>
+    void operator()(const T index) {
+        ids->operator[](index) = get_thread_id();
+    }
+};
+
 } // namespace utils
 
 #endif // GRAPH_DISPATCHING_TESTS_UTILS_HPP
