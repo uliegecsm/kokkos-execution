63e5a020df20eb8f075c05f1fd0c3986ed1b4396
romintomasetti <romin.tomasetti@gmail.com>
2025-03-06 17:29:16 +0000

    kokkos(ext): first step is to make a valid but useless scheduler

--- kokkos_ext/impl/ExecutionSpaceContext.hpp
+++ kokkos_ext/impl/ExecutionSpaceContext.hpp
@@ -3,6 +3,13 @@
 
 #include <concepts>
 
+#include "tests/IgnoreWarnings.hpp"
+PRAGMA_DIAGNOSTIC_PUSH
+PRAGMA_DIAGNOSTIC_IGNORED("-Wunused-parameter")
+PRAGMA_DIAGNOSTIC_IGNORED("-Wdeprecated-copy")
+#include <stdexec/execution.hpp>
+PRAGMA_DIAGNOSTIC_POP
+
 #include "Kokkos_Core.hpp"
 
 namespace Kokkos::Experimental
@@ -10,31 +17,39 @@ namespace Kokkos::Experimental
 
 namespace details::execution_space
 {
-//! See https://github.com/NVIDIA/stdexec/blob/9514e7bdf4b5d16d8ee4b5ad0e9c8733c3539f37/include/nvexec/stream/common.cuh#L168-L195).
-template <typename Exec> requires Kokkos::is_execution_space_v<Exec>
-struct ContextState
-{
-    Exec exec;
-};
-
-/**
- * @brief Scheduler for a @c Kokkos execution space.
- *
- * @warning It is a puppet and does not verify the @c std::execution::scheduler concept.
- */
+//! Scheduler for a @c Kokkos execution space.
 template <typename Exec> requires Kokkos::is_execution_space_v<Exec>
 struct ExecutionSpaceScheduler
 {
-    using context_state_t = ContextState<Exec>;
+    //! See https://github.com/NVIDIA/stdexec/blob/9514e7bdf4b5d16d8ee4b5ad0e9c8733c3539f37/include/nvexec/stream/common.cuh#L168-L195).
+    struct Env
+    {
+        //! The accepted completion tag types must agree with @c Sender::completion_signatures.
+        template <::stdexec::__one_of<::stdexec::set_value_t> CompletionTag>
+        auto query(stdexec::get_completion_scheduler_t<CompletionTag>) const noexcept { return ExecutionSpaceScheduler{exec}; }
 
-    context_state_t context_state;
+        bool operator==(const Env&) const noexcept = default;
+
+        Exec exec;
+    };
+
+    struct Sender
+    {
+        using sender_concept = stdexec::sender_t;
+
+        auto& get_env() const noexcept { return env; }
+
+        Env env;
+    };
 
     template <typename T>
-    explicit ExecutionSpaceScheduler(T&& exec) : context_state{std::forward<T>(exec)} {}
+    explicit ExecutionSpaceScheduler(T&& exec) : env{std::forward<T>(exec)} {}
 
-    /// @todo Return a sender instead of @p exec.
-    /// @todo Constraint the return type to be a sender.
-    auto& schedule() const { return context_state.exec; }
+    ::stdexec::sender auto schedule() const noexcept { return Sender{.env = env}; }
+
+    bool operator==(const ExecutionSpaceScheduler&) const noexcept = default;
+
+    Env env;
 };
 
 //! Deduction guide for @ref ExecutionSpaceScheduler.
@@ -55,7 +70,7 @@ struct ExecutionSpaceContext
 {
     Exec exec;
 
-    auto get_scheduler() { return details::execution_space::ExecutionSpaceScheduler{exec}; }
+    auto get_scheduler() const noexcept { return details::execution_space::ExecutionSpaceScheduler{exec}; }
 };
 
 } // namespace Kokkos::Experimental
