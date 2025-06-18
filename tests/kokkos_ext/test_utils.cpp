622eb4cc9165590305cc7afc7bb1cb8270bf9270
romintomasetti <romin.tomasetti@gmail.com>
2025-06-18 14:46:16 +0000

    kokkos(sha): update
    Signed-off-by: romintomasetti <romin.tomasetti@gmail.com>

--- tests/kokkos_ext/test_utils.cpp
+++ tests/kokkos_ext/test_utils.cpp
@@ -24,15 +24,15 @@ using execution_space = Kokkos::DefaultExecutionSpace;
  */
 TEST(update_policy, range_policy_exec)
 {
-    const auto execs = Kokkos::Experimental::partition_space(execution_space{}, 1, 1);
+    const auto [exec_a, exec_b] = Kokkos::Experimental::partition_space(execution_space{}, 1, 1);
 
-    ASSERT_NE(execs.at(0), execs.at(1));
+    ASSERT_NE(exec_a, exec_b);
 
-    const Kokkos::RangePolicy<execution_space> initial(execs.at(0), 0, 1, Kokkos::ChunkSize(2));
-    ASSERT_EQ(initial.space(), execs.at(0));
+    const Kokkos::RangePolicy<execution_space> initial(exec_a, 0, 1, Kokkos::ChunkSize(2));
+    ASSERT_EQ(initial.space(), exec_a);
 
-    const auto updated = Kokkos::Experimental::graph::details::update_policy(initial, execs.at(1));
-    ASSERT_EQ(updated.space(),      execs.at(1));
+    const auto updated = Kokkos::Experimental::graph::details::update_policy(initial, exec_b);
+    ASSERT_EQ(updated.space(),      exec_b);
     ASSERT_EQ(updated.begin(),      0);
     ASSERT_EQ(updated.end(),        1);
     ASSERT_EQ(updated.chunk_size(), 2);
