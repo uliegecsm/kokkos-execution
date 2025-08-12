1a814b1de99ff914c6b436e7bd5624cfaa9c3028
romintomasetti <romin.tomasetti@gmail.com>
2025-08-12 10:03:13 +0000

    cmake(stdexec): use cpm to get system context target as well
    Signed-off-by: romintomasetti <romin.tomasetti@gmail.com>

--- kokkos_ext/impl/ExecutionSpaceContext.hpp
+++ kokkos_ext/impl/ExecutionSpaceContext.hpp
@@ -7,6 +7,8 @@
 PRAGMA_DIAGNOSTIC_PUSH
 PRAGMA_DIAGNOSTIC_IGNORED("-Wunused-parameter")
 PRAGMA_DIAGNOSTIC_IGNORED("-Wdeprecated-copy")
+PRAGMA_DIAGNOSTIC_IGNORED("-Wshadow")
+PRAGMA_DIAGNOSTIC_IGNORED("-Wempty-body")
 #include <stdexec/execution.hpp>
 PRAGMA_DIAGNOSTIC_POP
 
