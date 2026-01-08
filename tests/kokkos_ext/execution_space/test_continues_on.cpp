f9a9f80d3c8def1158803dc81cc3404ff8d2d54d
romintomasetti <romin.tomasetti@gmail.com>
2026-01-08 18:07:22 +0000

    kokkos(continues_on): customization works for sync wait after then
    Signed-off-by: romintomasetti <romin.tomasetti@gmail.com>

--- tests/kokkos_ext/execution_space/test_continues_on.cpp
+++ tests/kokkos_ext/execution_space/test_continues_on.cpp
@@ -1,7 +1,8 @@
-#include "gtest/gtest.h"
+#include "kokkos-utils/callbacks/RecorderListener.hpp"
+#include "kokkos-utils/tests/scoped/callbacks/Manager.hpp"
 
+#include "tests/CallbackMatchers.hpp"
 #include "tests/kokkos_ext/execution_space/Helpers.hpp"
-
 #include "tests/stdexec/Utils.hpp"
 
 /**
@@ -21,8 +22,15 @@ using execution_space = Kokkos::DefaultExecutionSpace;
 namespace tests::kokkos_ext
 {
 
-class ContinuesOnTest : public impl::ExecutionSpaceContextTest<execution_space>
-{};
+using namespace Kokkos::utils::callbacks;
+
+class ContinuesOnTest : public impl::ExecutionSpaceContextTest<execution_space>,
+                        public Kokkos::utils::tests::scoped::callbacks::Manager
+{
+public:
+    using recorder_listener_t = RecorderListener<BeginFenceEvent, BeginParallelForEvent>;
+    using variant_t           = std::variant    <BeginFenceEvent, BeginParallelForEvent>;
+};
 
 //! @test Similar to @ref tests::stdexec::adaptors::ContinuesOnTest_no_schedule_sender_continues_on_Test.
 TEST_F(ContinuesOnTest, completing_domain)
@@ -30,14 +38,6 @@ TEST_F(ContinuesOnTest, completing_domain)
     const context_t esc{exec};
 
     ::stdexec::sender auto sndr = ::stdexec::just(42) | ::stdexec::continues_on(esc.get_scheduler());
-
-    int placeholder = 0;
-
-    auto opstate = ::stdexec::connect(std::move(sndr), ::tests::stdexec::value_receiver<int>{.value = std::addressof(placeholder)});
-
-    ::stdexec::start(opstate);
-
-    ASSERT_EQ(placeholder, 42);
 
     static_assert(std::same_as<
         ::stdexec::__domain_of_t<::stdexec::env_of_t<decltype(sndr)>>,
@@ -51,6 +51,63 @@ TEST_F(ContinuesOnTest, completing_domain)
 
     //! @todo This should be working once properly customized.
     static_assert(!::tests::stdexec::has_completion_scheduler_for<decltype(sndr), ::stdexec::set_value_t>);
+}
+
+//! @test A @c then and a @c sync_wait following a @c continues_on properly use the execution space instance.
+TEST_F(ContinuesOnTest, then_sync_wait)
+{
+    const view_s_t data(Kokkos::view_alloc(exec, "data - shared space"));
+
+    const context_t esc{exec};
+
+    ::stdexec::sender auto chain = ::stdexec::just() | ::stdexec::continues_on(esc.get_scheduler()) | ADD_THEN;
+
+    ASSERT_EQ(data(), 0) << "Eager execution is not allowed.";
+
+    ASSERT_THAT(
+        recorder_listener_t::record([chain = std::move(chain)] () mutable { ::stdexec::sync_wait(std::move(chain)); }),
+        ContainsInOrder<variant_t>(
+            MATCHER_FOR_BEGIN_PFOR (exec, dispatch_label(exec, "then")),
+            MATCHER_FOR_BEGIN_FENCE(exec, dispatch_label(exec, "sync_wait"))
+        )
+    );
+
+    ASSERT_EQ(data(), 1);
+}
+
+/**
+ * @test Check that @c continues_on is properly customized (with appropriate synchronization)
+ *       when using it many times on the same execution space instance.
+ *
+ * There shouldn't be any fencing required in this case.
+ */
+TEST_F(ContinuesOnTest, transition_to_same_execution_space_instance)
+{
+    const view_s_t data(Kokkos::view_alloc(exec, "data - shared space"));
+
+    const context_t esc{exec};
+
+    auto chain = ::stdexec::just()
+        | ::stdexec::continues_on(esc.get_scheduler())
+        | ADD_THEN
+        | ::stdexec::continues_on(esc.get_scheduler())
+        | ADD_THEN
+        | ::stdexec::continues_on(esc.get_scheduler())
+        | ADD_THEN;
+
+    ASSERT_EQ(data(), 0) << "Eager execution is not allowed.";
+
+    ASSERT_THAT(
+        recorder_listener_t::record([chain = std::move(chain)] () mutable { ::stdexec::sync_wait(std::move(chain)); }),
+        ::testing::ElementsAre(
+            MATCHER_FOR_BEGIN_PFOR (exec, dispatch_label(exec, "then")),
+            MATCHER_FOR_BEGIN_PFOR (exec, dispatch_label(exec, "then")),
+            MATCHER_FOR_BEGIN_PFOR (exec, dispatch_label(exec, "then")),
+            MATCHER_FOR_BEGIN_FENCE(exec, dispatch_label(exec, "sync_wait"))
+        )
+    );
+
+    ASSERT_EQ(data(), 3) << "A synchronization is missing.";
 }
 
 } // namespace tests::kokkos_ext
