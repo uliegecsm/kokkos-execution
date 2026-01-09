415e6aceb73a6120929bbd2b19d45ac3a0668c5e
romintomasetti <romin.tomasetti@gmail.com>
2026-01-09 17:14:29 +0000

    kokkos(stdexec): introduce concept `execution_space_completing_sender`
    Signed-off-by: romintomasetti <romin.tomasetti@gmail.com>

--- tests/Utils.hpp
+++ tests/Utils.hpp
@@ -8,6 +8,7 @@ PRAGMA_DIAGNOSTIC_PUSH
 PRAGMA_DIAGNOSTIC_IGNORED("-Wunused-parameter")
 PRAGMA_DIAGNOSTIC_IGNORED("-Wshadow")
 PRAGMA_DIAGNOSTIC_IGNORED("-Wempty-body")
+PRAGMA_DIAGNOSTIC_IGNORED("-Wswitch-default")
 #include "exec/static_thread_pool.hpp"
 PRAGMA_DIAGNOSTIC_POP
 
