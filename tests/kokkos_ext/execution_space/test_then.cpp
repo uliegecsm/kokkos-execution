51b52f8fe913f0b1784a45d20d251a3a5191e401
romintomasetti <romin.tomasetti@gmail.com>
2026-01-10 10:38:35 +0000

    stdexec(p3557): High-Quality Sender Diagnostics with Constexpr Exceptions
    Signed-off-by: romintomasetti <romin.tomasetti@gmail.com>

--- tests/kokkos_ext/execution_space/test_then.cpp
+++ tests/kokkos_ext/execution_space/test_then.cpp
@@ -117,7 +117,7 @@ TEST_F(ThenTest, then_starts_on)
     //! Until it is connected, the completion signatures are *dependent* (they are not fully known yet).
     static_assert(std::same_as<std::invoke_result_t<
         ::stdexec::get_completion_signatures_t, starts_on_t>,
-        ::stdexec::_ERROR_<::stdexec::__detail::__dependent_completions>
+        ::stdexec::_ERROR_<::stdexec::dependent_sender_error, ::stdexec::__errs::_WITH_SENDER_<::stdexec::__basic_sender<::stdexec::__starts_on_ns::starts_on_t, Kokkos::Experimental::details::execution_space::ExecutionSpaceScheduler<execution_space>, ::stdexec::__basic_sender<::stdexec::__then::then_t, tests::ThenFunctor<Kokkos::View<int, Kokkos::SharedSpace>>, ::stdexec::__basic_sender<::stdexec::__then::then_t, tests::ThenFunctor<Kokkos::View<int, Kokkos::SharedSpace>>, ::stdexec::__basic_sender<::stdexec::__just::just_t, ::stdexec::__tup::__tuple<>>>>>>>
     >);
 
     static_assert(std::same_as<std::invoke_result_t<
