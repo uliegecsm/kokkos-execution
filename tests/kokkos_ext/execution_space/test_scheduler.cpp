65194c47a5161b1e12ad21bbd39ccbde32fd346f
Maarten Arnst <maarten.arnst@uliege.be>
2026-01-14 16:10:54 +0100

    Minor changes execution space context
    Signed-off-by: Maarten Arnst <maarten.arnst@uliege.be>

--- tests/kokkos_ext/execution_space/test_scheduler.cpp
+++ tests/kokkos_ext/execution_space/test_scheduler.cpp
@@ -1,3 +1,5 @@
+#include "gtest/gtest.h"
+
 #include "kokkos_ext/impl/ExecutionSpaceContext.hpp"
 
 /**
@@ -9,7 +11,7 @@
  * This group of tests check that @ref Kokkos::Experimental::details::execution_space::Scheduler is a proper scheduler.
  *
  * The tests can be found in @ref tests/kokkos_ext/execution_space/test_scheduler.cpp.
- * 
+ *
  * References:
  *  * https://eel.is/c++draft/exec.sched
  */
@@ -18,12 +20,12 @@ using execution_space = Kokkos::DefaultExecutionSpace;
 
 namespace tests::kokkos_ext {
 
-using execution_space_scheduler_t =
-    decltype(std::declval<const Kokkos::Experimental::ExecutionSpaceContext<execution_space>&>().get_scheduler());
-using execution_space_env_t = Kokkos::Experimental::details::execution_space::SchedulerEnv<execution_space>;
+using execution_space_scheduler_t = Kokkos::Experimental::details::execution_space::Scheduler<execution_space>;
+using execution_space_scheduler_env_t = Kokkos::Experimental::details::execution_space::SchedulerEnv<execution_space>;
+using execution_space_schedule_sender_t = typename execution_space_scheduler_t::Sender;
 
-//! @test @ref Kokkos::Experimental::details::execution_space::Scheduler verifies @c stdexec::scheduler.
-constexpr bool test_is_a_scheduler() {
+//! @test @ref Kokkos::Experimental::details::execution_space::Scheduler models the @c stdexec::scheduler concept.
+constexpr bool test_scheduler_concept() {
     static_assert(stdexec::scheduler<execution_space_scheduler_t>);
 
     /**
@@ -34,9 +36,49 @@ constexpr bool test_is_a_scheduler() {
      *
      * Related to https://github.com/NVIDIA/stdexec/issues/1406.
      */
-    return std::derived_from<typename execution_space_scheduler_t::scheduler_concept, stdexec::scheduler_t>;
+    static_assert(std::derived_from<typename execution_space_scheduler_t::scheduler_concept, stdexec::scheduler_t>);
+
+    //! According to https://eel.is/c++draft/exec.sched#1, a @c schedule invocation must return a sender.
+    static_assert(stdexec::sender<decltype(stdexec::schedule(std::declval<const execution_space_scheduler_t&>()))>);
+
+    return true;
 }
-static_assert(test_is_a_scheduler());
+static_assert(test_scheduler_concept());
+
+/**
+ * @test Check that querying the completion scheduler from a schedule sender of a scheduler returns the scheduler.
+ * 
+ * See https://eel.is/c++draft/exec.sched#5.
+ */
+TEST(Scheduler, round_trip_property) {
+    const execution_space_scheduler_t sch{execution_space{}};
+    ASSERT_EQ(stdexec::get_completion_scheduler<stdexec::set_value_t>(stdexec::get_env(stdexec::schedule(sch))), sch);
+}
+
+/**
+ * @test Check that the @c schedule method of @ref Kokkos::Experimental::details::execution_space::Scheduler returns
+ *       @ref Kokkos::Experimental::details::execution_space::Scheduler::Sender.
+ */
+constexpr bool test_scheduler_schedule() {
+    static_assert(std::same_as<
+                  decltype(stdexec::schedule(std::declval<const execution_space_scheduler_t&>())),
+                  execution_space_schedule_sender_t
+    >);
+
+    return true;
+}
+
+/**
+ * @test Check that the @c stdexec::get_env query on @ref Kokkos::Experimental::details::execution_space::Scheduler::Sender returns
+ *       @ref Kokkos::Experimental::details::execution_space::SchedulerEnv.
+ */
+constexpr bool test_schedule_sender_env() {
+    static_assert(
+        std::same_as<stdexec::env_of_t<execution_space_schedule_sender_t>, const execution_space_scheduler_env_t&>);
+
+    return true;
+}
+static_assert(test_schedule_sender_env());
 
 //! @test Check @ref Kokkos::Experimental::details::execution_space::Scheduler queries.
 constexpr bool test_scheduler_queries() {
@@ -47,6 +89,7 @@ constexpr bool test_scheduler_queries() {
                   >,
                   Kokkos::Experimental::details::execution_space::Domain
     >);
+
     static_assert(std::same_as<
                   stdexec::__query_result_t<
                       execution_space_scheduler_t,
@@ -59,34 +102,26 @@ constexpr bool test_scheduler_queries() {
 }
 static_assert(test_scheduler_queries());
 
-/**
- * @test Check that the @c stdexec::get_env query on @ref Kokkos::Experimental::details::execution_space::Scheduler::Sender returns
- *       @ref Kokkos::Experimental::details::execution_space::SchedulerEnv.
- */
-constexpr bool test_scheduler_sender_get_env() {
-    static_assert(
-        std::same_as<stdexec::env_of_t<typename execution_space_scheduler_t::Sender>, const execution_space_env_t&>);
-
-    return true;
-}
-static_assert(test_scheduler_sender_get_env());
-
 //! @test Check queries of @ref Kokkos::Experimental::details::execution_space::SchedulerEnv.
-constexpr bool test_environment_queries() {
-    static_assert(
-        std::same_as<
-            stdexec::__query_result_t<execution_space_env_t, stdexec::get_completion_scheduler_t<stdexec::set_value_t>>,
-            execution_space_scheduler_t
-        >);
+constexpr bool test_scheduler_env_queries() {
+    static_assert(std::same_as<
+                  stdexec::__query_result_t<
+                      execution_space_scheduler_env_t,
+                      stdexec::get_completion_scheduler_t<stdexec::set_value_t>
+                  >,
+                  execution_space_scheduler_t
+    >);
 
-    static_assert(
-        std::same_as<
-            stdexec::__query_result_t<execution_space_env_t, stdexec::get_completion_domain_t<stdexec::set_value_t>>,
-            Kokkos::Experimental::details::execution_space::Domain
-        >);
+    static_assert(std::same_as<
+                  stdexec::__query_result_t<
+                      execution_space_scheduler_env_t,
+                      stdexec::get_completion_domain_t<stdexec::set_value_t>
+                  >,
+                  Kokkos::Experimental::details::execution_space::Domain
+    >);
 
     return true;
 }
-static_assert(test_environment_queries());
+static_assert(test_scheduler_env_queries());
 
 } // namespace tests::kokkos_ext
