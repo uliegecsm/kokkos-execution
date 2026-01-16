8fa51a85617e906e89758df959151628ff87978b
romintomasetti <romin.tomasetti@gmail.com>
2026-01-16 14:23:20 +0000

    tests(exec): repeat_effect_until
    Signed-off-by: romintomasetti <romin.tomasetti@gmail.com>

---
+++ tests/kokkos_ext/execution_space/test_repeat_effect_until.cpp
@@ -0,0 +1,74 @@
+#include "tests/IgnoreWarnings.hpp"
+PRAGMA_DIAGNOSTIC_PUSH
+PRAGMA_DIAGNOSTIC_IGNORED("-Wunused-parameter")
+PRAGMA_DIAGNOSTIC_IGNORED("-Wdeprecated-copy")
+PRAGMA_DIAGNOSTIC_IGNORED("-Wshadow")
+PRAGMA_DIAGNOSTIC_IGNORED("-Wempty-body")
+#include "exec/repeat_effect_until.hpp"
+PRAGMA_DIAGNOSTIC_POP
+
+#include "kokkos-utils/callbacks/RecorderListener.hpp"
+#include "kokkos-utils/tests/scoped/callbacks/Manager.hpp"
+
+#include "tests/CallbackMatchers.hpp"
+#include "tests/kokkos_ext/Helpers.hpp"
+#include "tests/kokkos_ext/execution_space/Helpers.hpp"
+#include "tests/stdexec/Utils.hpp"
+
+/**
+ * @addtogroup unittests
+ *
+ * Use @c Kokkos::Experimental::ExecutionSpaceContext with @c exec::repeat_effect_until
+ * ------------------------------------------------------------------------------------
+ *
+ * This group of tests check that @ref Kokkos::Experimental::ExecutionSpaceContext properly interacts with
+ * @c exec::repeat_effect_until.
+ *
+ * The tests can be found in @ref tests/kokkos_ext/execution_space/test_repeat_effect_until.cpp.
+ */
+
+using execution_space = Kokkos::DefaultExecutionSpace;
+
+namespace tests::kokkos_ext {
+
+using namespace Kokkos::utils::callbacks;
+
+class RepeatEffectUntilTest
+    : public impl::ExecutionSpaceContextTest<execution_space>
+    , public Kokkos::utils::tests::scoped::callbacks::Manager {
+   public:
+    using recorder_listener_t = RecorderListener<BeginFenceEvent, BeginParallelForEvent>;
+};
+
+//! @test Check that @ref Kokkos::Experimental::ExecutionSpaceContext can be properly embedded in a @c exec::repeat_effect_until.
+TEST_F(RepeatEffectUntilTest, works) {
+    const view_s_t data(Kokkos::view_alloc(exec, "data - shared space"));
+
+    const context_t esc{exec};
+
+    auto chain = ::stdexec::schedule(esc.get_scheduler()) | ADD_THEN
+               | ::stdexec::bulk(::stdexec::par, 2, BulkFunctor{.data = data});
+
+    ASSERT_THAT(
+        recorder_listener_t::record([chain = std::move(chain)]() mutable {
+            unsigned int guard = 0;
+            ::stdexec::sync_wait(
+                ::exec::repeat_effect_until(
+                    std::move(chain) | ::stdexec::continues_on(::stdexec::inline_scheduler{})
+                    | ::stdexec::then([&guard]() -> bool { return (++guard) >= 3; })));
+        }),
+        ::testing::ElementsAre(
+            MATCHER_FOR_BEGIN_PFOR(exec, dispatch_label(exec, "then")),
+            MATCHER_FOR_BEGIN_PFOR(exec, dispatch_label(exec, "bulk")),
+            MATCHER_FOR_BEGIN_FENCE(exec, dispatch_label(exec, "schedule_from")),
+            MATCHER_FOR_BEGIN_PFOR(exec, dispatch_label(exec, "then")),
+            MATCHER_FOR_BEGIN_PFOR(exec, dispatch_label(exec, "bulk")),
+            MATCHER_FOR_BEGIN_FENCE(exec, dispatch_label(exec, "schedule_from")),
+            MATCHER_FOR_BEGIN_PFOR(exec, dispatch_label(exec, "then")),
+            MATCHER_FOR_BEGIN_PFOR(exec, dispatch_label(exec, "bulk")),
+            MATCHER_FOR_BEGIN_FENCE(exec, dispatch_label(exec, "schedule_from"))));
+
+    ASSERT_EQ(data(), 6);
+}
+
+} // namespace tests::kokkos_ext
---
+++ tests/utils/Counter.hpp
@@ -0,0 +1,60 @@
+#ifndef GRAPH_DISPATCHING_TESTS_UTILS_COUNTER_HPP
+#define GRAPH_DISPATCHING_TESTS_UTILS_COUNTER_HPP
+
+#include <atomic>
+
+namespace tests::utils {
+
+//! Count construction, move/copy construction, move/copy assignment and destruction atomically on host.
+struct Counter {
+    static inline std::atomic<unsigned int> default_constructions{0};
+    static inline std::atomic<unsigned int> destructions{0};
+    static inline std::atomic<unsigned int> copy_constructions{0};
+    static inline std::atomic<unsigned int> copy_assignments{0};
+    static inline std::atomic<unsigned int> move_constructions{0};
+    static inline std::atomic<unsigned int> move_assignments{0};
+
+    unsigned int id;
+
+    Counter()
+        : id(default_constructions.fetch_add(1, std::memory_order_relaxed)) {
+    }
+
+    ~Counter() {
+        destructions.fetch_add(1, std::memory_order_relaxed);
+    }
+
+    Counter(const Counter&)
+        : id(copy_constructions.fetch_add(1, std::memory_order_relaxed)) {
+    }
+
+    Counter& operator=(const Counter&) {
+        copy_assignments.fetch_add(1, std::memory_order_relaxed);
+        return *this;
+    }
+
+    Counter(Counter&&) noexcept
+        : id(move_constructions.fetch_add(1, std::memory_order_relaxed)) {
+    }
+
+    Counter& operator=(Counter&&) noexcept {
+        move_assignments.fetch_add(1, std::memory_order_relaxed);
+        return *this;
+    }
+
+    void operator()() const {
+    }
+
+    static void reset() {
+        default_constructions = 0;
+        destructions = 0;
+        copy_constructions = 0;
+        copy_assignments = 0;
+        move_constructions = 0;
+        move_assignments = 0;
+    }
+};
+
+} // namespace tests::utils
+
+#endif // GRAPH_DISPATCHING_TESTS_UTILS_COUNTER_HPP
