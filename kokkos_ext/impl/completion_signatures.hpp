d6d2acb415a6faf033edcb15c822254b7cc8be61
Maarten Arnst <maarten.arnst@uliege.be>
2026-02-05 13:44:55 +0100

    kokkos(ext): shorten completion signatures machinery and add assertion
    Signed-off-by: Maarten Arnst <maarten.arnst@uliege.be>
    Signed-off-by: romintomasetti <romin.tomasetti@gmail.com>

--- kokkos_ext/impl/completion_signatures.hpp
+++ kokkos_ext/impl/completion_signatures.hpp
@@ -5,11 +5,31 @@
 
 namespace Kokkos::Experimental::details::impl {
 
-//! As required by https://github.com/NVIDIA/stdexec/commit/a0d95e90fc188f4f73328c4274551434edba3165, that follows from @cite P3557R3. // NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
-#define GRAPH_DISPATCHING_KOKKOS_EXT_COMPLETION_SIGNATURES(_type_)                                                     \
-    template <::stdexec::__decays_to<_type_> Self, typename... Env>                                                    \
-    static consteval auto get_completion_signatures() -> _completion_signatures<Self, Env...> {                        \
-        return {};                                                                                                     \
+/**
+ * @brief Completion signatures of @c _sndr_type_.
+ *
+ * The @c stdexec::set_value_t() completion signature is always added.
+ *
+ * References:
+ *  - https://github.com/NVIDIA/stdexec/commit/a0d95e90fc188f4f73328c4274551434edba3165
+ *  - @cite P3557R3.
+ */ // NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
+#define GRAPH_DISPATCHING_KOKKOS_EXT_COMPL_SIGS_ADD(_sndr_type_, ...)                                                  \
+    template <::stdexec::__decays_to<_sndr_type_> Self, typename... Env>                                               \
+    static consteval auto get_completion_signatures() {                                                                \
+        using child_completions_t =                                                                                    \
+            ::stdexec::__completion_signatures_of_t<::stdexec::__copy_cvref_t<Self, Sndr>, Env...>;                    \
+        return ::stdexec::transform_completion_signatures<                                                             \
+            child_completions_t,                                                                                       \
+            ::stdexec::completion_signatures<::stdexec::set_value_t() __VA_OPT__(, ) __VA_ARGS__>                      \
+        >{};                                                                                                           \
+    }
+
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
+#define GRAPH_DISPATCHING_KOKKOS_EXT_COMPL_SIGS_KEEP(_sndr_type_)                                                      \
+    template <::stdexec::__decays_to<_sndr_type_> Self, typename... Env>                                               \
+    static consteval auto get_completion_signatures() {                                                                \
+        return ::stdexec::__completion_signatures_of_t<::stdexec::__copy_cvref_t<Self, Sndr>, Env...>{};               \
     }
 
 } // namespace Kokkos::Experimental::details::impl
