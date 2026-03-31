2d56418d38ce99d99b32d3fa39061bd134c46744
romintomasetti <romin.tomasetti@gmail.com>
2026-03-31 10:25:41 +0000

    graph: adding custom events for better testing
    Signed-off-by: romintomasetti <romin.tomasetti@gmail.com>

---
+++ kokkos-execution/graph/events.hpp
@@ -0,0 +1,129 @@
+#ifndef KOKKOS_EXECUTION_GRAPH_EVENTS_HPP
+#define KOKKOS_EXECUTION_GRAPH_EVENTS_HPP
+
+#include "Kokkos_Core.hpp"
+#include "Kokkos_Graph.hpp"
+
+#if defined(KOKKOS_EXECUTION_ENABLE_EVENT_DISPATCH)
+#    include "kokkos-utils/callbacks/Manager.hpp"
+#endif
+
+namespace Kokkos::Execution::GraphImpl {
+
+//! Event to be sent to @ref Kokkos::utils::callbacks::dispatch when a @c Kokkos graph is created.
+struct GraphCreateEvent {
+    void* graph = nullptr;
+    uint32_t dev_id = 0;
+    uint64_t event_id = 0;
+
+    constexpr auto operator<=>(const GraphCreateEvent&) const = default;
+
+    friend std::ostream& operator<<(std::ostream& out, const GraphCreateEvent& event) {
+        return out << "GraphCreateEvent: {graph = " << event.graph << ", dev_id = " << event.dev_id
+                   << ", event_id = " << event.event_id << '}';
+    }
+};
+
+/**
+ * @brief Event to be sent to @ref Kokkos::utils::callbacks::dispatch when a @c Kokkos graph node is added.
+ *
+ * @todo Store a label.
+ * @todo Store the dispatch tag.
+ */
+struct GraphAddNodeEvent {
+    void* graph = nullptr;
+    void* predecessor = nullptr;
+    void* node = nullptr;
+    uint32_t dev_id = 0;
+
+    constexpr auto operator<=>(const GraphAddNodeEvent&) const = default;
+
+    friend std::ostream& operator<<(std::ostream& out, const GraphAddNodeEvent& event) {
+        return out << "GraphAddNodeEvent: {graph = " << event.graph << ", predecessor = " << event.predecessor
+                   << ", node = " << event.node << ", dev_id = " << event.dev_id << '}';
+    }
+};
+
+//! Event to be sent to @ref Kokkos::utils::callbacks::dispatch when a @c Kokkos graph is instantiated.
+struct GraphInstantiateEvent {
+    void* graph = nullptr;
+
+    constexpr auto operator<=>(const GraphInstantiateEvent&) const = default;
+
+    friend std::ostream& operator<<(std::ostream& out, const GraphInstantiateEvent& event) {
+        return out << "GraphInstantiateEvent: {graph = " << event.graph << '}';
+    }
+};
+
+//! Event to be sent to @ref Kokkos::utils::callbacks::dispatch when a @c Kokkos graph is submitted.
+struct GraphSubmitEvent {
+    void* graph = nullptr;
+    uint32_t dev_id = 0;
+
+    constexpr auto operator<=>(const GraphSubmitEvent&) const = default;
+
+    friend std::ostream& operator<<(std::ostream& out, const GraphSubmitEvent& event) {
+        return out << "GraphSubmitEvent: {graph = " << event.graph << ", dev_id = " << event.dev_id << '}';
+    }
+};
+
+//! Retrieve the raw graph pointer from a node.
+template <typename NodeType>
+requires Kokkos::Impl::is_specialization_of_v<NodeType, Kokkos::Experimental::GraphNodeRef>
+auto* get_graph_impl_ptr(const NodeType& node) noexcept {
+    return Kokkos::Impl::GraphAccess::get_graph_weak_ptr(node).lock().get();
+}
+
+//! Record a @ref GraphCreateEvent event.
+template <Kokkos::ExecutionSpace Exec>
+void graph_create_event(const Kokkos::Experimental::Graph<Exec>& graph) {
+#if defined(KOKKOS_EXECUTION_ENABLE_EVENT_DISPATCH)
+    Kokkos::utils::callbacks::dispatch(
+        GraphCreateEvent{
+            .graph = get_graph_impl_ptr(graph.root_node()),
+            .dev_id = Kokkos::Tools::Experimental::device_id(graph.get_device_handle().m_exec),
+            .event_id = Kokkos::utils::callbacks::get_next_event_id()});
+#endif
+}
+
+/**
+ * @brief Record an event for a @p node added after @p predecessor.
+ *
+ * @todo There is no way to tell if the predecessor is a root node.
+ */
+template <bool IsRoot, Kokkos::ExecutionSpace Exec, typename PredecessorRef, typename NodeRef>
+void graph_add_node_event(
+    const Kokkos::Impl::DeviceHandle<Exec>& handle,
+    const PredecessorRef& predecessor,
+    const NodeRef& node) {
+#if defined(KOKKOS_EXECUTION_ENABLE_EVENT_DISPATCH)
+    Kokkos::utils::callbacks::dispatch(
+        GraphAddNodeEvent{
+            .graph = get_graph_impl_ptr(predecessor),
+            .predecessor = IsRoot ? nullptr : Kokkos::Impl::GraphAccess::get_node_ptr(predecessor).get(),
+            .node = Kokkos::Impl::GraphAccess::get_node_ptr(node).get(),
+            .dev_id = Kokkos::Tools::Experimental::device_id(handle.m_exec)});
+#endif
+}
+
+//! Record a @ref GraphInstantiateEvent event.
+template <Kokkos::ExecutionSpace Exec>
+void graph_instantiate_event(const Kokkos::Experimental::Graph<Exec>& graph) {
+#if defined(KOKKOS_EXECUTION_ENABLE_EVENT_DISPATCH)
+    Kokkos::utils::callbacks::dispatch(GraphInstantiateEvent{.graph = get_graph_impl_ptr(graph.root_node())});
+#endif
+}
+
+
+//! Record a @ref GraphSubmitEvent event.
+template <Kokkos::ExecutionSpace Exec>
+void graph_submit_event(const Kokkos::Experimental::Graph<Exec>& graph, const Exec& exec) {
+#if defined(KOKKOS_EXECUTION_ENABLE_EVENT_DISPATCH)
+    Kokkos::utils::callbacks::dispatch(
+        GraphSubmitEvent{
+            .graph = get_graph_impl_ptr(graph.root_node()), .dev_id = Kokkos::Tools::Experimental::device_id(exec)});
+#endif
+}
+} // namespace Kokkos::Execution::GraphImpl
+
+#endif // KOKKOS_EXECUTION_GRAPH_EVENTS_HPP
---
+++ tests/graph/events.hpp
@@ -0,0 +1,79 @@
+#ifndef KOKKOS_EXECUTION_TESTS_GRAPH_EVENTS_HPP
+#define KOKKOS_EXECUTION_TESTS_GRAPH_EVENTS_HPP
+
+#include "kokkos-utils/callbacks/Helpers.hpp"
+
+#include "kokkos-execution/graph/events.hpp"
+
+namespace Tests::GraphImpl {
+
+DEFINE_EVENT_MATCHER_IN(Kokkos::Execution::GraphImpl, GraphAddNodeEvent)
+DEFINE_EVENT_MATCHER_IN(Kokkos::Execution::GraphImpl, GraphCreateEvent)
+DEFINE_EVENT_MATCHER_IN(Kokkos::Execution::GraphImpl, GraphInstantiateEvent)
+DEFINE_EVENT_MATCHER_IN(Kokkos::Execution::GraphImpl, GraphSubmitEvent)
+
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
+#define MATCHER_FOR_GRAPH_CREATE(_device_handle_)                                                                      \
+    AGraphCreateEvent(                                                                                                 \
+        ::testing::Field(                                                                                              \
+            &Kokkos::Execution::GraphImpl::GraphCreateEvent::dev_id,                                                   \
+            ::testing::Eq(Kokkos::Tools::Experimental::device_id(_device_handle_.m_exec))))
+
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
+#define MATCHER_FOR_GRAPH_ADDNODE(_graph_create_event_variant_, _device_handle_, _predecessor_)                        \
+    AGraphAddNodeEvent(                                                                                                \
+        ::testing::Field(                                                                                              \
+            &Kokkos::Execution::GraphImpl::GraphAddNodeEvent::dev_id,                                                  \
+            ::testing::Eq(Kokkos::Tools::Experimental::device_id(_device_handle_.m_exec))),                            \
+        ::testing::Field(                                                                                              \
+            &Kokkos::Execution::GraphImpl::GraphAddNodeEvent::graph,                                                   \
+            ::testing::Eq(                                                                                             \
+                std::get<Kokkos::Execution::GraphImpl::GraphCreateEvent>(_graph_create_event_variant_).graph)),        \
+        ::testing::Field(&Kokkos::Execution::GraphImpl::GraphAddNodeEvent::predecessor, ::testing::Eq(_predecessor_)))
+
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
+#define MATCHER_FOR_GRAPH_NODE_OF(_graph_add_node_event_)                                                              \
+    std::get<Kokkos::Execution::GraphImpl::GraphAddNodeEvent>(_graph_add_node_event_).node
+
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
+#define MATCHER_FOR_GRAPH_INSTANTIATE(_graph_create_event_variant_)                                                    \
+    AGraphInstantiateEvent(                                                                                            \
+        ::testing::Field(                                                                                              \
+            &Kokkos::Execution::GraphImpl::GraphInstantiateEvent::graph,                                               \
+            ::testing::Eq(                                                                                             \
+                std::get<Kokkos::Execution::GraphImpl::GraphCreateEvent>(_graph_create_event_variant_).graph)))
+
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
+#define MATCHER_FOR_GRAPH_SUBMIT(_exec_, _graph_create_event_variant_)                                                 \
+    AGraphSubmitEvent(                                                                                                 \
+        ::testing::Field(                                                                                              \
+            &Kokkos::Execution::GraphImpl::GraphSubmitEvent::dev_id,                                                   \
+            ::testing::Eq(Kokkos::Tools::Experimental::device_id(_exec_))),                                            \
+        ::testing::Field(                                                                                              \
+            &Kokkos::Execution::GraphImpl::GraphSubmitEvent::graph,                                                    \
+            ::testing::Eq(                                                                                             \
+                std::get<Kokkos::Execution::GraphImpl::GraphCreateEvent>(_graph_create_event_variant_).graph)))
+
+
+//! Similar to @ref EventDiscardMatcher, for graph-related events.
+template <Kokkos::ExecutionSpace Exec>
+struct GraphEventDiscardMatcher {
+    /**
+     * Filter out @ref Kokkos::utils::callbacks::BeginFenceEvent triggered by:
+     *  * https://github.com/kokkos/kokkos/blob/10e9786f862733fc7c7a23d5e998e3d161dd7b70/core/src/impl/Kokkos_Default_Graph_Impl.hpp#L144
+     *  * https://github.com/kokkos/kokkos/blob/10e9786f862733fc7c7a23d5e998e3d161dd7b70/core/src/HIP/Kokkos_HIP_Graph_Impl.hpp#L100
+     */
+    constexpr bool operator()(const Kokkos::utils::callbacks::BeginFenceEvent& event) const {
+        return event.name != "Kokkos::DefaultGraph::submit: fencing before launching graph nodes"
+            && event.name != "Kokkos::GraphImpl::~GraphImpl: Graph Destruction";
+    }
+
+    template <Kokkos::utils::callbacks::Event EventType>
+    constexpr bool operator()(const EventType&) const {
+        return true;
+    }
+};
+
+} // namespace Tests::GraphImpl
+
+#endif // KOKKOS_EXECUTION_TESTS_GRAPH_EVENTS_HPP
