93d28850ffc9f3a9dd6b38877b3a791d168ebe7c
Maarten Arnst <maarten.arnst@uliege.be>
2026-05-30 12:09:03 +0200

    impl(event): use a sender based event for hpx
    Signed-off-by: Maarten Arnst <maarten.arnst@uliege.be>
    Signed-off-by: romintomasetti <romin.tomasetti@gmail.com>

--- kokkos-execution/impl/HPX/event.hpp
+++ kokkos-execution/impl/HPX/event.hpp
@@ -18,9 +18,22 @@ namespace Kokkos::Execution::Impl {
 template <>
 struct HasNonBlockingDispatch<Kokkos::Experimental::HPX> : std::true_type { };
 
+/**
+ * This implementation assumes that once a call to @ref record is made,
+ * a call to @ref wait or @ref Kokkos::Execution::Impl::wait follows at some point.
+ *
+ * Indeed, since @ref record steals the @c Kokkos::Experimental::HPX instance sender, the only way to wait for the
+ * completion of the operations enqueued *before* the call to @ref record is to call @ref wait or @ref Kokkos::Execution::Impl::wait,
+ * *i.e.* stealing the instance sender has decoupled its operations from the @c Kokkos::Experimental::HPX instance.
+ *
+ * Therefore, after calling @ref record, the ordering relationship is no longer carried by the execution space instance;
+ * subsequently enqueued operations that need to be ordered after the previously submitted operations must depend on the event.
+ */
 template <>
 struct Event<Kokkos::Experimental::HPX> {
-    mutable std::optional<Kokkos::Experimental::HPX> m_exec = std::nullopt;
+    using sndr_t = hpx::execution::experimental::unique_any_sender<>;
+
+    mutable std::optional<sndr_t> m_sender;
     uint64_t m_event_id = invalid_event_id;
 
     Event() = default;
@@ -30,8 +43,17 @@ struct Event<Kokkos::Experimental::HPX> {
     Event& operator=(Event&&) noexcept = delete;
     ~Event() = default;
 
+    //! Steal the head of the underlying sender, and store it in @ref m_sender.
     void record(const Kokkos::Experimental::HPX& exec) {
-        m_exec = exec;
+        KOKKOS_EXPECTS(!m_sender.has_value());
+
+        auto& instance_data = exec.impl_get_instance_data();
+
+        const std::scoped_lock<hpx::spinlock> lock(instance_data.m_sender_mutex);
+
+        m_sender = std::move(instance_data.m_sender);
+
+        instance_data.m_sender = sndr_t(hpx::execution::experimental::just());
     }
 
     /**
@@ -42,29 +64,32 @@ struct Event<Kokkos::Experimental::HPX> {
      * @c hpx::lcos::local::event, has been discarded because it would never clean the underlying sender through a
      * @c fence.
      *
-     * Yet, it has been decided to mimic
-     * https://github.com/kokkos/kokkos/blob/91584fc13aaf09330bc391466dbae0249895291f/core/src/HPX/Kokkos_HPX.hpp#L139-L156
-     * so that there is no @c fence event recorded.
-     *
-     * Even though @ref wait may therefore synchronize even the work pushed to @ref m_exec after the call to @ref record,
-     * it's the most reasonable way forward for now.
+     * The current implementation sync-waits the value of @ref m_sender,
+     * and then resets it to @c std::nullopt so that all the previous
+     * operations in the sender get a chance to clean up.
      */
     void wait() const {
-        if (m_exec.has_value()) {
-            auto& instance_data = m_exec->impl_get_instance_data();
-
-            {
-                const std::lock_guard<hpx::spinlock> lock(instance_data.m_sender_mutex);
-
-                auto& sndr = instance_data.m_sender;
-                hpx::this_thread::experimental::sync_wait(std::move(sndr));
-                sndr = hpx::execution::experimental::unique_any_sender<>(hpx::execution::experimental::just());
-            }
-
-            m_exec = std::nullopt;
+        if (m_sender.has_value()) {
+            hpx::this_thread::experimental::sync_wait(std::move(*m_sender));
+            m_sender = std::nullopt;
         }
     }
 };
+
+//! Enqueue a call to @ref Event::wait into the underlying sender chain.
+template <>
+void impl_wait(const Kokkos::Experimental::HPX& exec, const Event<Kokkos::Experimental::HPX>& event) {
+    auto& instance_data = exec.impl_get_instance_data();
+
+    const std::scoped_lock<hpx::spinlock> lock(instance_data.m_sender_mutex);
+
+    auto& sndr = instance_data.m_sender;
+    sndr = hpx::execution::experimental::unique_any_sender<>{
+        hpx::execution::experimental::when_all(std::move(sndr), std::move(*event.m_sender))};
+
+    //! Mark the event as consumed.
+    event.m_sender = std::nullopt;
+}
 
 } // namespace Kokkos::Execution::Impl
 
